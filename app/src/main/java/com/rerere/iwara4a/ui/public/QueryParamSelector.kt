560bedcf467906445c1b6e576cc31a7fba0dab17
jiangdashao <qq1609403959@outlook.com>
2021-08-14 00:23:53 +0800

    fix: 修改排序和过滤对话框的标题

--- app/src/main/java/com/rerere/iwara4a/ui/public/QueryParamSelector.kt
+++ app/src/main/java/com/rerere/iwara4a/ui/public/QueryParamSelector.kt
@@ -1,10 +1,7 @@
 package com.rerere.iwara4a.ui.public
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -12,15 +9,12 @@ import androidx.compose.material.icons.filled.MenuOpen
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.flowlayout.FlowRow
-import com.google.android.material.chip.Chip
 import com.rerere.iwara4a.model.index.*
 import com.vanpra.composematerialdialogs.MaterialDialog
 import com.vanpra.composematerialdialogs.customView
-import com.vanpra.composematerialdialogs.listItemsSingleChoice
 import com.vanpra.composematerialdialogs.title
 
 @Composable
@@ -39,7 +33,7 @@ fun QueryParamSelector(
             }
         }
     ) {
-        title(text = "选择排序条件")
+        title(text = "排序和过滤")
         customView {
             var expand by remember {
                 mutableStateOf(false)
