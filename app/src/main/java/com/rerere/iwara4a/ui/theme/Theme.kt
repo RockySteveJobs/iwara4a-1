f69017abf7fe24192e2adcbb3a49e034a97de736
re-ovo <qq1609403959@outlook.com>
2022-03-25 13:13:37 +0800

    build: bump to next version 1.29

--- app/src/main/java/com/rerere/iwara4a/ui/theme/Theme.kt
+++ app/src/main/java/com/rerere/iwara4a/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import com.rerere.iwara4a.BuildConfig
 import com.rerere.iwara4a.ui.component.rememberStringPreference
 
 @Composable
@@ -34,33 +35,35 @@ fun Iwara4aTheme(
         }
     }
     SideEffect {
-        println("primary = Color(0x${colorScheme.primary.toHex()})")
-        println("onPrimary = Color(0x${colorScheme.onPrimary.toHex()})")
-        println("primaryContainer = Color(0x${colorScheme.primaryContainer.toHex()})")
-        println("onPrimaryContainer = Color(0x${colorScheme.onPrimaryContainer.toHex()})")
-        println("inversePrimary = Color(0x${colorScheme.inversePrimary.toHex()})")
-        println("secondary = Color(0x${colorScheme.secondary.toHex()})")
-        println("onSecondary = Color(0x${colorScheme.onSecondary.toHex()})")
-        println("secondaryContainer = Color(0x${colorScheme.secondaryContainer.toHex()})")
-        println("onSecondaryContainer = Color(0x${colorScheme.onSecondaryContainer.toHex()})")
-        println("tertiary = Color(0x${colorScheme.tertiary.toHex()})")
-        println("onTertiary = Color(0x${colorScheme.onTertiary.toHex()})")
-        println("tertiaryContainer = Color(0x${colorScheme.tertiaryContainer.toHex()})")
-        println("onTertiaryContainer = Color(0x${colorScheme.onTertiaryContainer.toHex()})")
-        println("background = Color(0x${colorScheme.background.toHex()})")
-        println("onBackground = Color(0x${colorScheme.onBackground.toHex()})")
-        println("surface = Color(0x${colorScheme.surface.toHex()})")
-        println("onSurface = Color(0x${colorScheme.onSurface.toHex()})")
-        println("surfaceVariant = Color(0x${colorScheme.surfaceVariant.toHex()})")
-        println("onSurfaceVariant = Color(0x${colorScheme.onSurfaceVariant.toHex()})")
-        println("surfaceTint = Color(0x${colorScheme.surfaceTint.toHex()})")
-        println("inverseSurface = Color(0x${colorScheme.inverseSurface.toHex()})")
-        println("inverseOnSurface = Color(0x${colorScheme.inverseOnSurface.toHex()})")
-        println("error = Color(0x${colorScheme.error.toHex()})")
-        println("onError = Color(0x${colorScheme.onError.toHex()})")
-        println("errorContainer = Color(0x${colorScheme.errorContainer.toHex()})")
-        println("onErrorContainer = Color(0x${colorScheme.onErrorContainer.toHex()})")
-        println("outline = Color(0x${colorScheme.outline.toHex()})")
+        if (BuildConfig.DEBUG) {
+            println("primary = Color(0x${colorScheme.primary.toHex()})")
+            println("onPrimary = Color(0x${colorScheme.onPrimary.toHex()})")
+            println("primaryContainer = Color(0x${colorScheme.primaryContainer.toHex()})")
+            println("onPrimaryContainer = Color(0x${colorScheme.onPrimaryContainer.toHex()})")
+            println("inversePrimary = Color(0x${colorScheme.inversePrimary.toHex()})")
+            println("secondary = Color(0x${colorScheme.secondary.toHex()})")
+            println("onSecondary = Color(0x${colorScheme.onSecondary.toHex()})")
+            println("secondaryContainer = Color(0x${colorScheme.secondaryContainer.toHex()})")
+            println("onSecondaryContainer = Color(0x${colorScheme.onSecondaryContainer.toHex()})")
+            println("tertiary = Color(0x${colorScheme.tertiary.toHex()})")
+            println("onTertiary = Color(0x${colorScheme.onTertiary.toHex()})")
+            println("tertiaryContainer = Color(0x${colorScheme.tertiaryContainer.toHex()})")
+            println("onTertiaryContainer = Color(0x${colorScheme.onTertiaryContainer.toHex()})")
+            println("background = Color(0x${colorScheme.background.toHex()})")
+            println("onBackground = Color(0x${colorScheme.onBackground.toHex()})")
+            println("surface = Color(0x${colorScheme.surface.toHex()})")
+            println("onSurface = Color(0x${colorScheme.onSurface.toHex()})")
+            println("surfaceVariant = Color(0x${colorScheme.surfaceVariant.toHex()})")
+            println("onSurfaceVariant = Color(0x${colorScheme.onSurfaceVariant.toHex()})")
+            println("surfaceTint = Color(0x${colorScheme.surfaceTint.toHex()})")
+            println("inverseSurface = Color(0x${colorScheme.inverseSurface.toHex()})")
+            println("inverseOnSurface = Color(0x${colorScheme.inverseOnSurface.toHex()})")
+            println("error = Color(0x${colorScheme.error.toHex()})")
+            println("onError = Color(0x${colorScheme.onError.toHex()})")
+            println("errorContainer = Color(0x${colorScheme.errorContainer.toHex()})")
+            println("onErrorContainer = Color(0x${colorScheme.onErrorContainer.toHex()})")
+            println("outline = Color(0x${colorScheme.outline.toHex()})")
+        }
     }
     MaterialTheme(
         colorScheme = colorScheme,
