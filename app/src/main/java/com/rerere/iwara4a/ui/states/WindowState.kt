990deeb68f20378aed41b3087b894b0f86cea34f
re-ovo <qq1609403959@outlook.com>
2022-05-04 22:25:08 +0800

    fix: 修复部分情况下窗口状态不更新的问题

--- app/src/main/java/com/rerere/iwara4a/ui/states/WindowState.kt
+++ app/src/main/java/com/rerere/iwara4a/ui/states/WindowState.kt
@@ -2,9 +2,7 @@ package com.rerere.iwara4a.ui.states
 
 import android.app.Activity
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.toComposeRect
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.DpSize
@@ -14,12 +12,12 @@ import androidx.window.layout.WindowMetricsCalculator
 @Composable
 fun rememberWindowSizeClass(): WindowSize {
     val activity = LocalContext.current as Activity
-    val configuration = LocalConfiguration.current
-    val windowMetrics = remember (configuration) {
+    // val configuration = LocalConfiguration.current
+    val windowMetrics = //remember(configuration) {
         WindowMetricsCalculator.getOrCreate()
             .computeCurrentWindowMetrics(activity)
-    }
-    val windowDpSize = with (LocalDensity.current) {
+    //}
+    val windowDpSize = with(LocalDensity.current) {
         windowMetrics.bounds.toComposeRect().size.toDpSize()
     }
     return when {
@@ -32,12 +30,10 @@ fun rememberWindowSizeClass(): WindowSize {
 @Composable
 fun rememberWindowDpSize(): DpSize {
     val activity = LocalContext.current as Activity
-    val configuration = LocalConfiguration.current
-    val windowMetrics = remember (configuration) {
-        WindowMetricsCalculator.getOrCreate()
-            .computeCurrentWindowMetrics(activity)
-    }
-    return with (LocalDensity.current) {
+    // val configuration = LocalConfiguration.current
+    val windowMetrics = WindowMetricsCalculator.getOrCreate()
+        .computeCurrentWindowMetrics(activity)
+    return with(LocalDensity.current) {
         windowMetrics.bounds.toComposeRect().size.toDpSize()
     }
 }
