1462622b636722d10409b971c693087cd233a3cc
re-ovo <qq1609403959@outlook.com>
2022-06-04 23:46:29 +0800

    build: 更新自动化测试规则，更新baseline配置

--- benchmark/src/main/java/com/rerere/iwara4a/benchmark/StartupBenchmark.kt
+++ benchmark/src/main/java/com/rerere/iwara4a/benchmark/StartupBenchmark.kt
@@ -6,6 +6,8 @@ import androidx.benchmark.macro.StartupMode
 import androidx.benchmark.macro.StartupTimingMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.Until
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -37,6 +39,14 @@ class StartupBenchmark {
         ) { // this = MacrobenchmarkScope
             pressHome()
             startActivityAndWait()
+            device.run {
+                waitForIdle()
+                wait(Until.findObject(By.desc("Media Preview Card")), 5_000)
+                findObjects(By.desc("Media Preview Card")).random().click()
+                waitForIdle()
+                pressBack()
+                waitForIdle()
+            }
         }
     }
 }
